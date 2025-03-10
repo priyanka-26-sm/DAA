95992eefa7642e7a18cef316d5203301739a061d
priyanka-26-sm <113653482+priyanka-26-sm@users.noreply.github.com>
2025-03-10 11:43:28 +0530

    Add files via upload

---
+++ search_sort_algo_in_java/bubble_sort.java
@@ -0,0 +1,39 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no swapping happened, the array is already sorted
+            if (!swapped) break;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
---
+++ search_sort_algo_in_java/insertion_sort.java
@@ -0,0 +1,36 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // Store the current element
+            int j = i - 1;
+
+            // Shift elements of arr[0..i-1] that are greater than key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]; // Move elements one position ahead
+                j--;
+            }
+
+            arr[j + 1] = key; // Place key in the correct position
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
